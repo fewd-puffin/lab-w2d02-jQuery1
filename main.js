8712bde75530a6c75a90ecc2b9cd50a25b24bcc2
trevorpreston <trevorpreston4@gmail.com>
2019-04-15 09:05:31 +0300

    add lab

---
+++ main.js
@@ -0,0 +1,11 @@
+// 1.  Select the H1
+let heading = "replace with jquery"
+console.log(heading)
+
+// 2.  Select all of the list items
+let listItems = "replace with jquery"
+console.log(listItems)
+
+// 3.  Select only the last list item
+let listItem = "replace with jquery"
+console.log(listItem)
